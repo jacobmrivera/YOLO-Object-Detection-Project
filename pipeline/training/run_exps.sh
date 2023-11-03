b48f9136b39ee574239f755e5b2d96529bd3ce12
Jacob Rivera <jakemichaelrivera32@gmail.com>
2023-11-03 15:23:04 -0500

    Update run_exps.sh

--- pipeline/training/run_exps.sh
+++ pipeline/training/run_exps.sh
@@ -1,26 +1,17 @@
  #! /bin/bash
 
-source "yolo_project\\venv\\Scripts\\activate"
+
+VIRTUAL_ENV_PATH=""
 
 
 SCRIPT="yolo_project\\detection_trainer.py"
-
 CONFIG_PATH="yolo_project\\config_inter_15.yaml"
-# CONFIG_PATH_B="yolo_project\\config_b.yaml"
-
 MODEL="yolov8s.pt"
 EPOCHS=600
 
+source $VIRTUAL_ENV_PATH
 
 python $SCRIPT --config_path $CONFIG_PATH --model $MODEL --epochs $EPOCHS
-# python $SCRIPT --config_path $CONFIG_PATH_B --model $MODEL --epochs $EPOCHS
-
-# MODEL="yolov8m.pt"
-# python $SCRIPT --config_path $CONFIG_PATH_A --model $MODEL --epochs $EPOCHS
-# python $SCRIPT --config_path $CONFIG_PATH_B --model $MODEL --epochs $EPOCHS
-
 
 deactivate
-#  cd ..\\..\\..
-
 echo "done..."
