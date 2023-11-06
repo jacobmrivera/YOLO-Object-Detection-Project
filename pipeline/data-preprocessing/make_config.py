20ba5ce31c7b0c0c5090b29ed723ba849fdae127
Jacob Rivera <64997622+jacobmrivera@users.noreply.github.com>
2023-11-06 15:19:10 -0600

    Update make_config.py

--- pipeline/data-preprocessing/make_config.py
+++ pipeline/data-preprocessing/make_config.py
@@ -1,15 +1,23 @@
-def make_config(object_names_file, out_name, dataset_path):
+import runner
 
-    config = open(f'{out_name}.yaml','w')
+def make_config():
+    out_name = runner.json_config["training"]["data"]
+    dataset_path = runner.json_config["dataset_folder"]
+
+    config = open(f'{out_name}','w')
     config.write(f'path: {dataset_path}\n')
     config.write('train: train\n')
     config.write('val: test\n')
     config.write('names:\n')
 
-    f = open(object_names_file,'r')
+    # f = open(object_names_file,'r')
 
-    for i, line in enumerate(f):
-        config.write(f'  {i}: {line.strip()}\n')
+    # for i, line in enumerate(f):
+    #     config.write(f'  {i}: {line.strip()}\n')
     
-    f.close()
+    # f.close()
+    obj_num = runner.json_config["constants"]["NUM_OBJS"]
+    for i in range(obj_num):
+        config.write(f'\t{i}: {runner.json_config["objects"][str(i)]}\n')
+
     config.close()
