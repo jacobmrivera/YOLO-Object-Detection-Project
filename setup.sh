b9e75df5a8fbf74143d716912a2314424ca64ef3
Jacob Rivera <64997622+jacobmrivera@users.noreply.github.com>
2024-01-17 13:34:05 -0600

    Fix bin file and update setup shell script

--- setup.sh
+++ setup.sh
@@ -1,23 +1,49 @@
 #!/bin/bash
 
-VENV_DIR="venv"
+# VENV_DIR="venv"
 
-# Check if the virtual environment directory exists
-if [ ! -d "$VENV_DIR" ]; then
+# # Check if the virtual environment directory exists
+# if [ ! -d "$VENV_DIR" ]; then
+#     echo "Creating virtual environment..."
+#     python -m venv $VENV_DIR
+# fi
+
+# # Activate the virtual environment
+# source $VENV_DIR/bin/activate
+
+# # Install dependencies from requirements.txt
+# if [ -f "requirements.txt" ]; then
+#     echo "Installing dependencies..."
+#     pip install -r requirements.txt
+# else
+#     echo "No requirements.txt file found."
+# fi
+
+# # Deactivate the virtual environment
+# deactivate
+
+#!/bin/bash
+
+# Check if venv directory exists
+if [ ! -d "venv" ]; then
     echo "Creating virtual environment..."
-    python -m venv $VENV_DIR
+    python -m venv venv
 fi
 
-# Activate the virtual environment
-source $VENV_DIR/bin/activate
-
-# Install dependencies from requirements.txt
-if [ -f "requirements.txt" ]; then
-    echo "Installing dependencies..."
-    pip install -r requirements.txt
+# Activate virtual environment based on the operating system
+if [[ "$OSTYPE" == "darwin"* || "$OSTYPE" == "linux-gnu" ]]; then
+    source venv/bin/activate
+elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+    source venv\\Scripts\\activate
+    echo "venv activated!"
 else
-    echo "No requirements.txt file found."
+    echo "Unsupported operating system. Please activate the virtual environment manually."
+    exit 1
 fi
 
-# Deactivate the virtual environment
+python -m pip install --upgrade pip
+# # Install requirements
+pip install -r requirements.txt
+
+# Deactivate virtual environment
 deactivate
