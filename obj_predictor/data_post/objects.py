a543afe1bc9490766613a6a88d9afc6e4f87eef3
Jacob Rivera <64997622+jacobmrivera@users.noreply.github.com>
2024-02-22 16:45:22 -0600

    update objects.py

--- obj_predictor/data_post/objects.py
+++ obj_predictor/data_post/objects.py
@@ -1,7 +1,7 @@
 # "z:\Jacob\YOLO_Predicted\20221112_10041_cam07_predicted_data\pred_labels_w_conf"
 
 import os
-
+import shutil
 from obj_predictor.data_processing import smooth
 from tqdm import tqdm
 
@@ -20,14 +20,14 @@ Second:
 '''
 
 def create_sub_obj_txts(output_dir, num_objs):
-    for i in range(1, num_objs + 1):
+    for i in range(num_objs):
         file_name = os.path.join(output_dir, f"{i}.txt")
         with open(file_name, 'w') as file:
             file.write("")
 
 
 def create_exp_obj_csvs(output_dir, num_objs):
-    for i in range(1, num_objs + 1):
+    for i in range(num_objs):
         file_name = os.path.join(output_dir, f"{i}.csv")
         with open(file_name, 'w') as file:
             file.write("")
@@ -41,11 +41,21 @@ def list_directories(path):
     return directories
 
 
+def append_files_method2(file1_path, file2_path):
+    with open(file1_path, 'r') as file1:
+        with open(file2_path, 'a') as file2:
+            shutil.copyfileobj(file1, file2)
+
+
+
 def gather_obj_lvl_predictions(input_dir, output_dir, num_objs):
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
+    
+
     # create a text file for each object in output_dir
+    print("Creating object text files...")
     create_sub_obj_txts(output_dir, num_objs)
 
     # subs = list_directories(input_dir)
@@ -53,24 +63,6 @@ def gather_obj_lvl_predictions(input_dir, output_dir, num_objs):
     frames_path = os.path.join(input_dir, "pred_labels_w_conf")
     # get list of all frames, order doesn't matter?
     frames_list = smooth.list_files_in_directory(frames_path, '.txt')
-
-    # # loop though all frames,
-    # for frame in tqdm(frames_list, desc="Processing frames"):
-    #     # loop through lines of file
-    #     with open(os.path.join(frames_path, frame), 'r') as file:
-    #         for line in file:
-    #             # Do something with each line, for example, print it
-    #              line = line.strip()
-    #              obj_num =  line.split(' ')[0]
-    #              new_line = line + f"_{frame}\n"
-                
-    #             # opens obj_lvl text file and appends
-    #              with open(os.path.join(output_dir, obj_num + ".txt"), 'a') as file:
-    #                 file.write(new_line)
-    #         # append line to its obj file
-
-
-
     
     # Dictionary to store file handles
     file_handles = {}
@@ -80,7 +72,7 @@ def gather_obj_lvl_predictions(input_dir, output_dir, num_objs):
             with open(os.path.join(frames_path, frame), 'r') as file:
                 for line in file:
                     line = line.strip()
-                    obj_num = int(line.split(' ')[0]) + 1
+                    obj_num = int(line.split(' ')[0])
                     new_line = line + f" _{frame}\n"
                     
                     # Check if file handle already exists, if not open the file and store the handle
@@ -97,20 +89,43 @@ def gather_obj_lvl_predictions(input_dir, output_dir, num_objs):
 
 
 def concat_obj_lvl_pred_across_subs(input_dir, output_dir, num_objs):
-    print()
-
-    # generate output csvs for each 
-
-    # given directory full of subject dirs, loop thoough
-        # loop throug all objs
-            # for every line in the obj text file, append it to the csv, structing it if needed
     
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+        
+    
+    # create a text file for each object in output_dir
+    print("Creating object text files...")
+    create_sub_obj_txts(output_dir, num_objs)
+
+
+    dir_list = [d for d in os.listdir(input_dir) if os.path.isdir(os.path.join(input_dir, d))]
+
+    for dir in tqdm(dir_list, desc="Processing directories..."):
+        print()
+        dir_full_path = os.path.join(input_dir, dir)
+        if not os.path.isdir(dir_full_path):
+            continue
+
+        # put subject's obj lvl data in subject folder
+        gather_obj_lvl_predictions(os.path.join(input_dir,dir), os.path.join(input_dir, dir), num_objs)
+
+        # Iterate through every subject's object files
+        for i in range(num_objs):
+
+            in_txt_path = os.path.join(input_dir, dir, f"{i}.txt")
+            out_txt_path = os.path.join(output_dir, f"{i}.txt")
+
+            append_files_method2(in_txt_path, out_txt_path)
+
+    
+    # generate output csvs for each 
 
 
 def main():
 
-    p = "z:\\Jacob\\YOLO_Predicted\\20221112_10041_cam07_predicted_data"
-    gather_obj_lvl_predictions(p, p, 27)
+    p = "z:\\Jacob\\YOLO_Predicted"
+    concat_obj_lvl_pred_across_subs(p, p, 27)
 
 if __name__ == "__main__":
     main()
