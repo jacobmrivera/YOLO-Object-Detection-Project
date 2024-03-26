b48f6a067998371a06774b29afb3b99c7cdd3934
Jacob Rivera <jakemichaelrivera32@gmail.com>
2024-03-26 11:49:30 -0500

    Add small changes to predict_frames.py and util.py

--- examples/predict_frames.py
+++ examples/predict_frames.py
@@ -6,6 +6,7 @@ from obj_predictor.predictor import PredictorModel  # Importing the PredictorMod
 from obj_predictor.data import DataMaster
 from pathlib import Path
 
+import obj_predictor.util as util
 
 model_path = Path("All_Data_Trainings\\all_data_2_14_mirrored_v8m\\weights\\best.pt")
 frames_path = Path("G:\\jacob\\practice_data_yolo\\short.mp4")
@@ -20,19 +21,23 @@ def main():
     THEN DRAW
     THEN STITCH
 
-    the full pipeline doesn't need the stitcing, just te smoothed annotations
+    the full pipeline doesn't need the stitching, just te smoothed annotations
         but I need to test it by drawing and stitching 
     '''
     # predictor.predict_video(input_video, save_annot=True)
 
     dataHandler = DataMaster(model_path)
 
-    labels_path = input_video.parent / "pred_labels"
-    # dataHandler.smooth_annotations(input_dir=labels_path)
+    labels_path = frames_path.parent / frames_path.name + "pred_labels"
+    output_dir = frames_path.parent / frames_path.name + "pred_frames"
 
-    dataHandler.batch_draw_bb( input_video, labels_path, labels_path)
-    
-    
+    dataHandler.smooth_annotations(input_dir=labels_path)
+    dataHandler.batch_draw_bb( frames_path, labels_path, output_dir)
+
+    util.frames_to_video(frames_path, output_video_path, fps=30):
+
+
+
 
 if __name__ == "__main__":
     main()
